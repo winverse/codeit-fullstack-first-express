2add1ec61f0e2a18cc5eee2892b6546a7ada4ce5
winverse <public.winverse@gmail.com>
2025-10-17 16:16:04 +0900

    feat: remove allow all origin

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -9,7 +9,6 @@ export const cors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
-  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE, OPTIONS',
